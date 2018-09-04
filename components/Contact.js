1b332a5f72e8389222912fa16b13d8e8b779bbb4
Dominik Soboń <dominik.sobon@gmail.com>
2018-09-04 18:51:36 +0200

    Add style.css

--- components/Contact.js
+++ components/Contact.js
@@ -5,14 +5,14 @@ var Contact = React.createClass({
 
   render: function() {
     return (
-      React.createElement('div', {className: 'contactItem'},
+      React.createElement('div', {className: 'contactItem col-12'},
         React.createElement('img', {
           className: 'contactImage',
           src: 'http://icons.veryicon.com/png/System/Qetto%202/contacts.png'
         }),
-        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-        React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+        React.createElement('p', {className: 'contactLabel col-3'}, 'Imię: ' + this.props.item.firstName),
+        React.createElement('p', {className: 'contactLabel col-3'}, 'Nazwisko: ' + this.props.item.lastName),
+        React.createElement('a', {className: 'contactEmail col-3', href: 'mailto:' + this.props.item.email},
           this.props.item.email
         )
       )
--- components/ContactForm.js
+++ components/ContactForm.js
@@ -10,18 +10,21 @@ var ContactForm = React.createClass({
           type: 'text',
           placeholder: 'Imię',
           value: this.props.contact.firstName,
+		  className: 'form-control',
         }),
         React.createElement('input', {
           type: 'text',
           placeholder: 'Nazwisko',
           value: this.props.contact.lastName,
+		  className: 'form-control',
         }),
         React.createElement('input', {
           type: 'email',
           placeholder: 'Email',
           value: this.props.contact.email,
+		  className: 'form-control',
         }),
-        React.createElement('button', {type: 'submit'}, "Dodaj kontakt")
+        React.createElement('button', {type: 'submit', className: 'btn btn-light'}, "Dodaj kontakt")
       )
     )
   },
